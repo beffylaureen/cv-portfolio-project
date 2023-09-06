cd915fd5b8c9b72c174b218baa93ef91489650eb
B Laureen <beffylaureen@me.com>
2023-09-06 15:47:28 -0500

    imported more images

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,4 +1,10 @@
 import React from "react";
+import Weather from "../assets/weather-dashboard.png";
+import Quiz from "../assets/code-quiz.png";
+import Happy from "../assets/happy-hour.png";
+import SVG from "../assets/svg-logo-maker.png";
+import Password from "../assets/password-generator.png";
+import Note from "../assets/note-taker.png";
 
 function Portfolio() {
   return (
@@ -7,7 +13,7 @@ function Portfolio() {
       <div className="row">
         <div className="col-md-4">
           <div className="card">
-            <img src="../../src/assets/weather-dashboard.png" className="card-img-top" alt="Weather Dashboard" />
+            <img src={Weather} className="card-img-top" alt="Weather Dashboard" />
             <div className="card-body">
               <h5 className="card-title">Weather Dashboard</h5>
               <p className="card-text">Displays current and future weather conditions.</p>
@@ -19,7 +25,7 @@ function Portfolio() {
         {/* Add more project cards or thumbnails */}
         <div className="col-md-4">
           <div className="card">
-            <img src="../../src/assets/code-quiz.png" className="card-img-top" alt="Code Quiz" />
+            <img src={Quiz} className="card-img-top" alt="Code Quiz" />
             <div className="card-body">
               <h5 className="card-title">Code Quiz</h5>
               <p className="card-text">A simple quiz about miscellaneous coding terms.</p>
@@ -30,7 +36,7 @@ function Portfolio() {
         </div>
         <div className="col-md-4">
           <div className="card">
-            <img src="../../src/assets/happy-hour.png" className="card-img-top" alt="Happy Hour" />
+            <img src={Happy} className="card-img-top" alt="Happy Hour" />
             <div className="card-body">
               <h5 className="card-title">Happy Hour</h5>
               <p className="card-text">A cocktail recipe generator.  This was a group project with Andrew, Jon, and Wyatt.</p>
@@ -41,7 +47,7 @@ function Portfolio() {
         </div>
         <div className="col-md-4">
           <div className="card">
-            <img src="../../src/assets/svg-logo-maker.png" className="card-img-top" alt="SVG Logo Maker" />
+            <img src={SVG}className="card-img-top" alt="SVG Logo Maker" />
             <div className="card-body">
               <h5 className="card-title">SVG Logo Maker</h5>
               <p className="card-text">A Node.js application that generates shapes containing three characters.</p>
@@ -52,7 +58,7 @@ function Portfolio() {
         </div>
         <div className="col-md-4">
           <div className="card">
-            <img src="../../src/assets/password-generator.png" className="card-img-top" alt="Code Quiz" />
+            <img src={Password} className="card-img-top" alt="Code Quiz" />
             <div className="card-body">
               <h5 className="card-title">Password Generator</h5>
               <p className="card-text">For those times when you need to come up with a new password, this application will generate one for you!</p>
@@ -63,7 +69,7 @@ function Portfolio() {
         </div>
         <div className="col-md-4">
           <div className="card">
-            <img src="../../src/assets/note-taker.png" className="card-img-top" alt="Note Taker" />
+            <img src={Note} className="card-img-top" alt="Note Taker" />
             <div className="card-body">
               <h5 className="card-title">Note Taker</h5>
               <p className="card-text">A note-taking application that utilizes an Express.js back end.</p>
